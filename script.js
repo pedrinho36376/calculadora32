597233dcae02daea4db5303e4d6e7dc614ae6b40
pedrinho36376 <145363027+pedrinho36376@users.noreply.github.com>
2023-11-13 11:38:26 -0300

    calculadora1533

---
+++ script.js
@@ -0,0 +1,10 @@
+let expression
+
+function appendNumber(appendNumberm) {
+    expression  += Number  
+    document.getElementById(´result´).value=expression
+
+
+    function appendOperator (appendOperator
+        e)
+}
